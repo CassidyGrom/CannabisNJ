b248c59ebdb48919963d5408091d267691938569
Cassidy Grom <cgrom@njadvancemedia.com>
2020-01-17 11:20:08 -0500

    finalish button

---
+++ script.js
@@ -0,0 +1,15 @@
+//Get the button
+var mybutton = document.getElementById("sticky");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
